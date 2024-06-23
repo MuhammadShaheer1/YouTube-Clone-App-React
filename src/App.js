99d92f5cdc1afca49498d834f95720d7b40e5d64
MuhammadShaheer1 <muhammadshaheer75@gmail.com>
2024-06-23 23:11:45 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { Grid } from "@material-ui/core";
+
+import { SearchBar, VideoList, VideoDetail } from './components';
+
+import youtube from './api/youtube';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Grid justify="center" container spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            {/* SEARCH BAR */}
+                        </Grid>
+                        <Grid item xs={8}>
+                            {/* VIDEO DETAILS */}
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/* VIDEOS LIST */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default App;
