986916d1d05619bf53f2bc9b7fbc908fd193dfc6
MuhammadShaheer1 <muhammadshaheer75@gmail.com>
2024-06-24 18:13:21 +0500

    Project Completed. Added new React Components

--- src/App.js
+++ src/App.js
@@ -2,24 +2,49 @@ import React from "react";
 
 import { Grid } from "@material-ui/core";
 
-import { SearchBar, VideoList, VideoDetail } from './components';
+import { SearchBar, VideoDetail, VideoList } from "./components";
 
 import youtube from './api/youtube';
 
 class App extends React.Component {
+    state = {
+        videos: [],
+        selectedVideo: null,
+    }
+
+    componentDidMount(){
+        this.handleSubmit('Youtube');
+    }
+
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video});
+    }
+
+    handleSubmit = async (searchTerm) => {
+        const response = await youtube.get('search', 
+            {params: {
+                part: 'snippet',
+                maxResults: 5,
+                key: 'AIzaSyBnlECpp76ZVYsDLk_EAUS6zEEQ_G7CEAk',
+                q: searchTerm,
+            }});
+            this.setState({ videos: response.data.items, selectedVideo: response.data.items[0]});
+    }
+
     render() {
+        const {selectedVideo, videos} = this.state
         return (
-            <Grid justify="center" container spacing={16}>
+            <Grid justify-content="center" container spacing={16}>
                 <Grid item xs={12}>
                     <Grid container spacing={16}>
                         <Grid item xs={12}>
-                            {/* SEARCH BAR */}
+                            <SearchBar onFormSubmit={this.handleSubmit}/>
                         </Grid>
                         <Grid item xs={8}>
-                            {/* VIDEO DETAILS */}
+                            <VideoDetail video={selectedVideo}/>
                         </Grid>
                         <Grid item xs={4}>
-                            {/* VIDEOS LIST */}
+                            <VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
                         </Grid>
                     </Grid>
                 </Grid>
